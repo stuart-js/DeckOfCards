b5ec0ebbc0b41c52e624239a27b4d97017738d1f
Stuart <sshelley@ualberta.ca>
2023-09-10 21:22:38 -0600

    update buildDeck method

--- DeckOfCards.py
+++ DeckOfCards.py
@@ -4,18 +4,28 @@ class CardDeck():
 
     def __init__(self) -> None:
         #self.cards = # list of cards in deck
+        #self.topCard = #TODO - complete top and bottom cards
+        #self.bottomCard = 
         pass
 
+    def buildDeck(self):
+        # Iterate through each suit
+        # Link each card with and previous
+        
+        self.topCard = Card("Ace", "Clubs", False)
+        self.bottomCard = Card("King", "Spades", True)
+
+    
 
 class Card():
     # should each card be an object?
     # Should also function as a linked list?
     isCard = True
 
-    def __init__(self, face: bool, value: int, suit: str) -> None:
-        self.face = face
+    def __init__(self, value: int, suit: str, face: bool) -> None:
         self.value = value
         self.suit = suit
+        self.face = face
         #self.prevCard = 
         #self.nextCard =
 
@@ -28,11 +38,6 @@ class Card():
 
         return string
     
-
-
-
-
-
 
 
 
@@ -50,7 +55,7 @@ class Card():
 
 def main():
     
-    eight_hearts = Card(False, 8, "Hearts")
+    eight_hearts = Card(8, "Hearts", False)
     
     print(eight_hearts) # This just prints object, need actual values
                         # Need to use __str__ method to return what you want to print of class
@@ -60,7 +65,22 @@ def main():
     # Each card can be a node of a linked list.
 
     #print(str(eight_hearts.isCard))
+
+    deck = CardDeck()
+    deck.buildDeck()
     
+    #TESTS
+    # Test topCard
+    if str(deck.topCard) == "Ace of Clubs\nFaceCard: False":
+        print("Passed")
+    else:
+        print("Failed")
+
+    # Test bottomCard
+    if str(deck.bottomCard) == "King of Spades\nFaceCard: True":
+        print("Passed")
+    else:
+        print("Failed")
 
 if __name__ == "__main__":
     main()
